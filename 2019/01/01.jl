17b800999db87a2f711d1d7e21ecc576cca507c8
dgkf <18220321+dgkf@users.noreply.github.com>
2019-11-30 21:41:45 -0800

    add day 1 solution

---
+++ 2019/01/01.jl
@@ -0,0 +1,18 @@
+# Advent of Code
+# 2019-12-01
+
+@assert isfile("./01.jl") "text input must be saved in '../01/data.txt'"
+module_masses = parse.(Int64, readlines("data.txt"))
+
+# part 1
+fuel = sum(div.(module_masses, 3) .- 2)
+println("Part 1 Fuel Required: $fuel")
+
+# part 2
+calc_fuel_required = function(mass)
+    fuel = div(mass, 3) - 2
+    fuel <= 0 ? 0 : fuel + calc_fuel_required(fuel)
+end
+
+fuel = sum(calc_fuel_required.(module_masses))
+println("Part 2 Fuel Required: $fuel")
