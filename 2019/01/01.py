a3b2db81a8ef7bc2786f8306375299f336e7c05e
dgkf <18220321+dgkf@users.noreply.github.com>
2019-11-30 22:15:19 -0800

    added python solution

---
+++ 2019/01/01.py
@@ -0,0 +1,20 @@
+# Advent of Code
+# 2019-01-01
+
+import os
+
+assert os.path.isfile("data.txt"), "input must be saved in '2019/01/data.txt'"
+with open("data.txt") as f:
+    module_masses = [int(mass) for mass in f.readlines()]
+
+# part 1
+fuel = sum([mass // 3 - 2 for mass in module_masses])
+print(f"Part 1 Fuel Required: {fuel}")
+
+# part 2
+def calc_fuel_required(mass):
+    fuel = mass // 3 - 2
+    return 0 if fuel <= 0 else fuel + calc_fuel_required(fuel)
+
+fuel = sum([calc_fuel_required(mass) for mass in module_masses])
+print(f"Part 2 Fuel Required: {fuel}")
