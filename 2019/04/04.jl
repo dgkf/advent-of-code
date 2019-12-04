8b760242bc35bf707ad0b9790e5a7273bb883b9f
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-03 21:42:36 -0800

    added part 1

--- 2019/04/04.jl
+++ 2019/04/04.jl
@@ -3,6 +3,33 @@ input = readlines()
 
 lb, ub = parse.(Int, split(input, "-"))
 
+
+# part 1
+n = 0
+for i=lb:ub
+    is_increasing = true
+    has_sequential_pair = false
+
+    d = reverse(digits(i, base = 10))
+    
+    for di=2:length(d)
+        if d[di] < d[di-1]
+            is_increasing = false
+            break
+        elseif d[di] == d[di-1]
+            has_sequential_pair = true
+        end
+    end
+
+    if is_increasing && has_sequential_pair
+        global n += 1
+    end
+end
+
+println(n)
+
+
+# part 2
 n = 0
 for i=lb:ub
     is_increasing = true
@@ -12,16 +39,15 @@ for i=lb:ub
     d = reverse(digits(i, base = 10))
     
     for di=2:length(d)
-        if (d[di] < d[di-1]) 
+        if d[di] < d[di-1]
             is_increasing = false
             break
-        elseif (d[di] == d[di-1]) 
+        elseif d[di] == d[di-1]
             sequential_count += 1
         end
 
-        if (d[di] != d[di-1]  || di == length(d)) && sequential_count == 2
+        if (d[di] != d[di-1] || di == length(d)) && sequential_count == 2
             has_sequential_pair = true
-            sequential_count = 1
         end
 
         if  d[di] != d[di-1]
@@ -33,3 +59,5 @@ for i=lb:ub
         global n += 1
     end
 end
+
+println(n)
