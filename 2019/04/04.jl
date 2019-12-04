f80f26948f1e73efa4fa92898984032a50983d72
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-04 10:31:21 -0800

    cleaned up sum and hcat use

--- 2019/04/04.jl
+++ 2019/04/04.jl
@@ -4,9 +4,11 @@ input = readlines()
 @assert(length(input) > 0, "puzzle input must be passed to stdin.")
 
 lb, ub = parse.(Int, split(input[1], "-"))
-println(join(sum(hcat(map(lb:ub) do i
+
+# part 2
+println(join(sum(map(lb:ub) do i
 	d = digits(i, base = 10)
 	c = counter(d)
 	all(diff(d) .<= 0) .& [any(values(c) .>= 2), 2 in values(c)]
-end...), dims = 2), "\n"))
+end), "\n"))
 
