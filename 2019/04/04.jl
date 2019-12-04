07b3b021f7e73b5bc0b94b27fc02c3bb9eae3b41
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-04 10:09:54 -0800

    drastically simplified solution :)

--- 2019/04/04.jl
+++ 2019/04/04.jl
@@ -1,63 +1,19 @@
+using DataStructures
+
 input = readlines()
 @assert(length(input) > 0, "puzzle input must be passed to stdin.")
 
 lb, ub = parse.(Int, split(input[1], "-"))
 
-
 # part 1
-n = 0
-for i=lb:ub
-    is_increasing = true
-    has_sequential_pair = false
-
-    d = reverse(digits(i, base = 10))
-    
-    for di=2:length(d)
-        if d[di] < d[di-1]
-            is_increasing = false
-            break
-        elseif d[di] == d[di-1]
-            has_sequential_pair = true
-        end
-    end
-
-    if is_increasing && has_sequential_pair
-        global n += 1
-    end
-end
-
-println(n)
-
+println(sum(map(lb:ub) do i
+    d = digits(i, base = 10)
+	all(diff(d) .<= 0) && any(values(counter(d)) .>= 2)
+end))
 
 # part 2
-n = 0
-for i=lb:ub
-    is_increasing = true
-    sequential_count = 1
-    has_sequential_pair = false
+println(sum(map(lb:ub) do i
+    d = digits(i, base = 10)
+	all(diff(d) .<= 0) && 2 in values(counter(d))
+end))
 
-    d = reverse(digits(i, base = 10))
-    
-    for di=2:length(d)
-        if d[di] < d[di-1]
-            is_increasing = false
-            break
-        elseif d[di] == d[di-1]
-            sequential_count += 1
-        end
-
-        if (d[di] != d[di-1] || di == length(d)) && sequential_count == 2
-            has_sequential_pair = true
-        end
-
-        if  d[di] != d[di-1]
-            sequential_count = 1
-        end
-    end
-
-    if is_increasing && has_sequential_pair
-        global n += 1
-    end
-end
-
-println(n)
