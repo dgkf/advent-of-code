1774d2182af35099006efef652909f061409a044
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-04 10:26:31 -0800

    merged loops

--- 2019/04/04.jl
+++ 2019/04/04.jl
@@ -4,16 +4,9 @@ input = readlines()
 @assert(length(input) > 0, "puzzle input must be passed to stdin.")
 
 lb, ub = parse.(Int, split(input[1], "-"))
-
-# part 1
-println(sum(map(lb:ub) do i
+println(join(sum(hcat(map(lb:ub) do i
 	d = digits(i, base = 10)
-	all(diff(d) .<= 0) && any(values(counter(d)) .>= 2)
-end))
-
-# part 2
-println(sum(map(lb:ub) do i
-	d = digits(i, base = 10)
-	all(diff(d) .<= 0) && 2 in values(counter(d))
-end))
+	c = counter(d)
+	all(diff(d) .<= 0) .& [any(values(c) .>= 2), 2 in values(c)]
+end...), dims = 2), "\n"))
 
