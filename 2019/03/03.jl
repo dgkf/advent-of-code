a3c8fe71658712de0e00104fd766411635b1394a
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-03 16:47:49 -0800

    moved to coord struct

--- 2019/03/03.jl
+++ 2019/03/03.jl
@@ -1,26 +1,27 @@
 input = readlines()
 @assert(length(input) > 0, "puzzle input must be passed to stdin.")
 
-struct Coord
-    x 
-    y
-    z
-end
+# define a coordinate type as syntactic sugar around an array (point.x)
+# it's much slower, but also easier to keep track of
+struct Coord; vec :: AbstractArray; end
+Base.:+(a::Coord, b::Coord) = Coord(a.vec + b.vec)
+Base.getproperty(coord::Coord, x::Symbol) = coord_prop(coord, Val(x), x)
+coord_prop(coord::Coord, sym::Val{:x}, x) = coord.vec[1]
+coord_prop(coord::Coord, sym::Val{:y}, x) = coord.vec[2]
+coord_prop(coord::Coord, sym, x) = getfield(coord, x)
 
-Base.:+(a::Coord, b::Coord) = Coord(a.x + b.x, a.y + b.y, a.z + b.z)
-
-ps = map(input) do line
+(lineA, lineB) = map(input) do line
     cumsum(map(split(line, ",")) do inst
         val = parse(Int, inst[2:end])
-        if     inst[1] == 'R'; Coord(val, 0, val)
-        elseif inst[1] == 'L'; Coord(-val, 0, val)
-        elseif inst[1] == 'U'; Coord(0, val, val)
-        elseif inst[1] == 'D'; Coord(0, -val, val)
+        if     inst[1] == 'R'; Coord([val, 0, val])
+        elseif inst[1] == 'L'; Coord([-val, 0, val])
+        elseif inst[1] == 'U'; Coord([0, val, val])
+        elseif inst[1] == 'D'; Coord([0, -val, val])
         end
     end)
 end
 
-find_intersection = function(a1, a2, b1, b2)
+function find_intersection(a1, a2, b1, b2)
     x = ((a2.x * a1.y - a1.x * a2.y) * (b2.x - b1.x) - (b2.x * b1.y - b1.x * b2.y) * (a2.x - a1.x)) / 
         ((a2.x - a1.x) * (b2.y - b1.y) - (b2.x - b1.x) * (a2.y - a1.y))
     y = ((a2.x * a1.y - a1.x * a2.y) * (b2.y - b1.y) - (b2.x * b1.y - b1.x * b2.y) * (a2.y - a1.y)) / 
@@ -36,38 +37,25 @@ find_intersection = function(a1, a2, b1, b2)
     end
 end
 
-find_min_distance = function(ps)
-    min_distance = Inf
-    for i=1:(length(ps[1])-1), j=1:(length(ps[2])-1)
-        cross = find_intersection(ps[1][i], ps[1][i+1], ps[2][j], ps[2][j+1])
-
-        if cross !== nothing
-            dist = sum(abs.(cross))
-            if dist < min_distance
-                min_distance = dist
-            end
-        end
-    end
-    return trunc(Int, min_distance)
-end
-
-find_min_path_dist = function(ps)
+function path_stats(lineA, lineB)
+    min_manh_dist = Inf
     min_path_dist = Inf
-    for i=1:(length(ps[1])-1), j=1:(length(ps[2])-1)
-        cross = find_intersection(ps[1][i], ps[1][i+1], ps[2][j], ps[2][j+1])
+    for i=1:(length(lineA)-1), j=1:(length(lineB)-1)
+        cross = find_intersection(lineA[i], lineA[i+1], lineB[j], lineB[j+1])
+        if cross === nothing; continue; end
 
-        if cross !== nothing
-            path_dist = ps[1][i].z + ps[2][j].z + 
-                sum(abs.(cross - [ps[1][i].x, ps[1][i].y])) + 
-                sum(abs.(cross - [ps[2][j].x, ps[2][j].y]))
+        # calculate manhattan distance
+        manh_dist = sum(abs.(cross))
+        min_manh_dist = manh_dist < min_manh_dist ? manh_dist : min_manh_dist
 
-            if path_dist < min_path_dist
-                min_path_dist = path_dist
-            end
-        end
+        # calculate path length
+        path_dist = lineA[i].vec[3] + lineB[j].vec[3] + 
+            sum(abs.(cross - [lineA[i].x, lineA[i].y])) + 
+            sum(abs.(cross - [lineB[j].x, lineB[j].y]))
+        min_path_dist = path_dist < min_path_dist ? path_dist : min_path_dist
     end
-    return trunc(Int, min_path_dist)
+    return trunc(Int, min_manh_dist), trunc(Int, min_path_dist)
 end
 
-println(find_min_distance(ps))
-println(find_min_path_dist(ps))
+println(join(path_stats(lineA, lineB), "\n"))
+
