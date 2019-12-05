a0076bddad851654e58da233b970fea2828cf7ea
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-05 15:56:08 -0800

    refactored opcodes

--- 2019/05/05.jl
+++ 2019/05/05.jl
@@ -1,76 +1,38 @@
 input = readlines()
 @assert(length(input) > 0, "puzzle input must be passed to stdin.")
 
-mem_in = parse.(Int64, split(input[1], ","))
+# create 0-indexed memory type
+struct MemoryArray; vec :: AbstractArray; end
+Base.copy(m::MemoryArray) = MemoryArray(copy(m.vec))
+Base.getindex(m::MemoryArray, i) = m.vec[i.+1]
+Base.getindex(m::MemoryArray, i, memcode) = mem_access(m, i, memcode)
+Base.setindex!(m::MemoryArray, val, i) = setindex!(m.vec, val, i+1)
+mem_access(m, i, imm) = (imm ? i : m[i])+1 > length(m.vec) ? 0 : imm ? m[i] : m[m[i]]
 
-access(mem, i, is_immediate) = is_immediate == 1 ? mem[i] : mem[mem[i]+1]
+# define opcode behaviors
+opcode(m, i) = rem(m[i], 100)
+params(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
+op!(m, i) = op!(Val(opcode(m,i)), m, i, mem_access.([m], i.+(1:3), params(m,i)))
+op!(c::Val{1}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] + ps[2], m[i+3]))
+op!(c::Val{2}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] * ps[2], m[i+3]))
+op!(c::Val{3}, m, i, ps::Array{Int}) = (i+2, setindex!(m, popfirst!(op_stdin), m[i+1]))
+op!(c::Val{4}, m, i, ps::Array{Int}) = (i+2, push!(op_stdout, ps[1]))
+op!(c::Val{5}, m, i, ps::Array{Int}) = (ps[1] != 0 ? ps[2] : i+3, nothing)
+op!(c::Val{6}, m, i, ps::Array{Int}) = (ps[1] == 0 ? ps[2] : i+3, nothing)
+op!(c::Val{7}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] < ps[2], m[i+3]))
+op!(c::Val{8}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] == ps[2], m[i+3]))
+runprog(m; i=0) = while opcode(m, i) != 99; i, _ = op!(m, i); end
 
-op_add! = function(m, i, modes)
-    m[m[i+3]+1] = access(m, i+1, modes[1]) + access(m, i+2, modes[2])
-    return i+4
-end
-
-op_mul! = function(m, i, modes)
-    m[m[i+3]+1] = access(m, i+1, modes[1]) * access(m, i+2, modes[2])
-    return i+4
-end
-
-op_store! = function(m, i, modes)
-    m[m[i+1]+1] = op_stdin[1]
-    return i+2
-end
-
-op_output! = function(m, i, modes)
-    push!(op_stdout, access(m, i+1, modes[1]))
-    return i+2
-end
-
-op_jump_if_true! = function(m, i, modes)
-    access(m, i+1, modes[1]) == 0 ? i+3 : access(m, i+2, modes[2])+1
-end
-
-op_jump_if_false! = function(m, i, modes)
-    access(m, i+1, modes[1]) == 0 ? access(m, i+2, modes[2])+1 : i+3
-end
-
-op_less_than! = function(m, i, modes)
-    m[m[i+3]+1] = access(m, i+1, modes[1]) < access(m, i+2, modes[2]) ? 1 : 0
-    return i+4
-end
-
-op_equals! = function(m, i, modes)
-    m[m[i+3]+1] = access(m, i+1, modes[1]) == access(m, i+2, modes[2]) ? 1 : 0
-    return i+4
-end
-
-insts = Dict(
-    1 => op_add!, 
-    2 => op_mul!, 
-    3 => op_store!,
-    4 => op_output!,
-    5 => op_jump_if_true!,
-    6 => op_jump_if_false!,
-    7 => op_less_than!,
-    8 => op_equals!
-)
-
-do_op! = function(m, i)
-    modes = digits.(div(m[i], 100), base = 10, pad = 3)
-    op = rem(m[i], 100)
-    insts[op](m, i, modes)
-end
-
-run_program = function(mem)
-    i = 1
-    while rem(mem[i], 100) != 99; i = do_op!(mem, i); end
-end
+# read input as MemoryArray
+mem = MemoryArray(parse.(Int64, split(input[1], ",")))
 
 # part 1
 op_stdin, op_stdout = [1], []
-run_program(copy(mem_in))
+runprog(copy(mem))
 println(last(op_stdout))
 
 # part 2
 op_stdin, op_stdout = [5], []
-run_program(copy(mem_in))
+runprog(copy(mem))
 println(last(op_stdout))
+
