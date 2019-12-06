253ab01dc06e0b5d9157fba4459085f51dacfa77
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-05 16:23:26 -0800

    cleaned up type annotations

--- 2019/05/05.jl
+++ 2019/05/05.jl
@@ -7,20 +7,20 @@ Base.copy(m::MemoryArray) = MemoryArray(copy(m.vec))
 Base.getindex(m::MemoryArray, i) = m.vec[i.+1]
 Base.getindex(m::MemoryArray, i, memcode) = mem_access(m, i, memcode)
 Base.setindex!(m::MemoryArray, val, i) = setindex!(m.vec, val, i+1)
-mem_access(m, i, imm) = (imm ? i : m[i])+1 > length(m.vec) ? 0 : imm ? m[i] : m[m[i]]
+access(m, i, imm) = (imm ? i : m[i])+1 > length(m.vec) ? 0 : imm ? m[i] : m[m[i]]
 
 # define opcode behaviors
 opcode(m, i) = rem(m[i], 100)
 params(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
-op!(m, i) = op!(Val(opcode(m,i)), m, i, mem_access.([m], i.+(1:3), params(m,i)))
-op!(c::Val{1}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] + ps[2], m[i+3]))
-op!(c::Val{2}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] * ps[2], m[i+3]))
-op!(c::Val{3}, m, i, ps::Array{Int}) = (i+2, setindex!(m, popfirst!(op_stdin), m[i+1]))
-op!(c::Val{4}, m, i, ps::Array{Int}) = (i+2, push!(op_stdout, ps[1]))
-op!(c::Val{5}, m, i, ps::Array{Int}) = (ps[1] != 0 ? ps[2] : i+3, nothing)
-op!(c::Val{6}, m, i, ps::Array{Int}) = (ps[1] == 0 ? ps[2] : i+3, nothing)
-op!(c::Val{7}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] < ps[2], m[i+3]))
-op!(c::Val{8}, m, i, ps::Array{Int}) = (i+4, setindex!(m, ps[1] == ps[2], m[i+3]))
+op!(m, i) = op!(Val(opcode(m,i)), m, i, access.([m], i.+(1:3), params(m,i)))
+op!(c::Val{1}, m, i, ps) = (i+4, setindex!(m, ps[1] + ps[2], m[i+3]))
+op!(c::Val{2}, m, i, ps) = (i+4, setindex!(m, ps[1] * ps[2], m[i+3]))
+op!(c::Val{3}, m, i, ps) = (i+2, setindex!(m, popfirst!(op_stdin), m[i+1]))
+op!(c::Val{4}, m, i, ps) = (i+2, push!(op_stdout, ps[1]))
+op!(c::Val{5}, m, i, ps) = (ps[1] != 0 ? ps[2] : i+3, nothing)
+op!(c::Val{6}, m, i, ps) = (ps[1] == 0 ? ps[2] : i+3, nothing)
+op!(c::Val{7}, m, i, ps) = (i+4, setindex!(m, ps[1] < ps[2], m[i+3]))
+op!(c::Val{8}, m, i, ps) = (i+4, setindex!(m, ps[1] == ps[2], m[i+3]))
 runprog(m; i=0) = while opcode(m, i) != 99; i, _ = op!(m, i); end
 
 # read input as MemoryArray
