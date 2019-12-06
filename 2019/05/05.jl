c29f4f9e9b998eb5361e7f87e25b28584c53efae
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-05 17:30:58 -0800

    cleaned up some variable names

--- 2019/05/05.jl
+++ 2019/05/05.jl
@@ -1,7 +1,5 @@
-input = readlines()
-@assert(length(input) > 0, "puzzle input must be passed to stdin.")
-
-memory = parse.(Int64, split(input[1], ","))
+# read memory from stdin
+memory = parse.(Int64, split(readlines()[1], ","))
 
 # create 0-indexed memory type
 struct MemoryArray; vec :: AbstractArray; end
@@ -15,18 +13,18 @@ mem_access(m, i, immediate) = immediate ? m[i] : m[m[i]]
 opcode(m, i) = rem(m[i], 100)
 params(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
 op!(m, i) = op!(Val(opcode(m,i)), m, i, mem_access.([m], i.+(1:3), params(m,i)))
-op!(c::Val{1}, m, i, ps) = (setindex!(m, ps[1] + ps[2], m[i+3]); i+4)
-op!(c::Val{2}, m, i, ps) = (setindex!(m, ps[1] * ps[2], m[i+3]); i+4)
-op!(c::Val{3}, m, i, ps) = (setindex!(m, popfirst!(op_stdin), m[i+1]); i+2)
-op!(c::Val{4}, m, i, ps) = (push!(op_stdout, ps[1]); i+2)
-op!(c::Val{5}, m, i, ps) = (ps[1] != 0 ? ps[2] : i+3)
-op!(c::Val{6}, m, i, ps) = (ps[1] == 0 ? ps[2] : i+3)
-op!(c::Val{7}, m, i, ps) = (setindex!(m, ps[1] < ps[2], m[i+3]); i+4)
-op!(c::Val{8}, m, i, ps) = (setindex!(m, ps[1] == ps[2], m[i+3]); i+4)
+op!(c::Val{1}, m, i, params) = (m[m[i+3]] = params[1] + params[2]; i+4)
+op!(c::Val{2}, m, i, params) = (m[m[i+3]] = params[1] * params[2]; i+4)
+op!(c::Val{3}, m, i, params) = (m[m[i+1]] = popfirst!(op_stdin); i+2)
+op!(c::Val{4}, m, i, params) = (push!(op_stdout, params[1]); i+2)
+op!(c::Val{5}, m, i, params) = (params[1] != 0 ? params[2] : i+3)
+op!(c::Val{6}, m, i, params) = (params[1] == 0 ? params[2] : i+3)
+op!(c::Val{7}, m, i, params) = (m[m[i+3]] = params[1] < params[2]; i+4)
+op!(c::Val{8}, m, i, params) = (m[m[i+3]] = params[1] == params[2]; i+4)
 
 # program execution
-exec(m; i=0) = while opcode(m, i) != 99
-	i = op!(m, i)
+exec(memory; idx = 0) = while opcode(memory, idx) != 99
+	idx = op!(memory, idx)
 end
 
 # part 1
