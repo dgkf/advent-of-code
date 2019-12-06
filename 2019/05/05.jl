523d2afb23eea016052f704244bd6c9db3da9afb
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-05 18:21:59 -0800

    cleaned up param handling, moved stdin/stdout into arguments

--- 2019/05/05.jl
+++ 2019/05/05.jl
@@ -3,32 +3,26 @@ using OffsetArrays
 # read memory from stdin
 memory = OffsetArray(parse.(Int64, split(readlines()[1], ",")), -1)
 
-# immediate and parameter mode memory reading with bounded indices
-mem_access(m, i, immediate) = immediate ? get(m, i, 0) : get(m, m[i], 0)
-
-# define opcode behaviors
+# opcode interpretation 
 opcode(m, i) = rem(m[i], 100)
-params(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
-op!(m, i) = op!(Val(opcode(m,i)), m, i, mem_access.([m], i.+(1:3), params(m,i)))
-op!(c::Val{1}, m, i, params) = (m[m[i+3]] = params[1] + params[2]; i+4)
-op!(c::Val{2}, m, i, params) = (m[m[i+3]] = params[1] * params[2]; i+4)
-op!(c::Val{3}, m, i, params) = (m[m[i+1]] = popfirst!(prog_stdin); i+2)
-op!(c::Val{4}, m, i, params) = (push!(prog_stdout, params[1]); i+2)
-op!(c::Val{5}, m, i, params) = (params[1] != 0 ? params[2] : i+3)
-op!(c::Val{6}, m, i, params) = (params[1] == 0 ? params[2] : i+3)
-op!(c::Val{7}, m, i, params) = (m[m[i+3]] = params[1] < params[2]; i+4)
-op!(c::Val{8}, m, i, params) = (m[m[i+3]] = params[1] == params[2]; i+4)
+modes(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
+param(m, i, immediate) = immediate ? get(m, i, 0) : get(m, m[i], 0)
+params(m, i) = param.([m], i .+ (1:3), modes(m, i))
+
+# opcode implementations
+op!(m, i, si, so) = op!(Val(opcode(m,i)), m, i, params(m, i), si, so)
+op!(c::Val{1}, m, i, params, si, so) = (m[m[i+3]] = params[1] + params[2]; i+4)
+op!(c::Val{2}, m, i, params, si, so) = (m[m[i+3]] = params[1] * params[2]; i+4)
+op!(c::Val{3}, m, i, params, si, so) = (m[m[i+1]] = popfirst!(si); i+2)
+op!(c::Val{4}, m, i, params, si, so) = (push!(so, params[1]); i+2)
+op!(c::Val{5}, m, i, params, si, so) = (params[1] != 0 ? params[2] : i+3)
+op!(c::Val{6}, m, i, params, si, so) = (params[1] == 0 ? params[2] : i+3)
+op!(c::Val{7}, m, i, params, si, so) = (m[m[i+3]] = params[1] < params[2]; i+4)
+op!(c::Val{8}, m, i, params, si, so) = (m[m[i+3]] = params[1] == params[2]; i+4)
 
 # program execution
-run(memory; i=0) = while opcode(memory, i) != 99; i = op!(memory, i); end
+run(m, si, so; i=0) = (while opcode(m, i) != 99; i = op!(m, i, si, so); end; so)
 
-# part 1
-prog_stdin, prog_stdout = [1], []
-run(copy(memory))
-println(last(prog_stdout))
-
-# part 2
-prog_stdin, prog_stdout = [5], []
-run(copy(memory))
-println(last(prog_stdout))
+println(last(run(copy(memory), [1], [])))
+println(last(run(copy(memory), [5], [])))
 
