fd899a36bf2313d29ae33895f8e87361ce52218f
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-05 19:27:01 -0800

    cleaned up immediate call code

--- 2019/05/05.jl
+++ 2019/05/05.jl
@@ -6,7 +6,7 @@ memory = OffsetArray(parse.(Int64, split(readlines()[1], ",")), -1)
 # opcode interpretation 
 opcode(m, i) = rem(m[i], 100)
 modes(m, i) = BitArray(digits.(div(m[i], 100); base=10, pad=3))
-param(m, i, immediate) = immediate ? get(m, i, 0) : get(m, m[i], 0)
+param(m, i, immediate) = get(m, immediate ? i : m[i], 0)
 params(m, i) = param.([m], i .+ (1:3), modes(m, i))
 
 # opcode implementations
@@ -21,9 +21,9 @@ op!(c::Val{7}, m, i, params, si, so) = (m[m[i+3]] = params[1] < params[2]; i+4)
 op!(c::Val{8}, m, i, params, si, so) = (m[m[i+3]] = params[1] == params[2]; i+4)
 
 # program execution
-run(m, si, so; i=0) = (while opcode(m, i) != 99; i = op!(m, i, si, so); end; so)
+run(m, si, so=[]; i=0) = (while opcode(m, i) != 99; i = op!(m, i, si, so); end; so)
 
 # run part 1 & 2 inputs and print last stdout entries
-println(last(run(copy(memory), [1], [])))
-println(last(run(copy(memory), [5], [])))
+println(last(run(copy(memory), [1])))
+println(last(run(copy(memory), [5])))
 
