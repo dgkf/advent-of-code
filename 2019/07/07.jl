8b180fd2cde44117201dbe312830c8d4ab92043b
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-07 15:15:25 -0800

    removed named IntCodeVM module prefix

--- 2019/07/07.jl
+++ 2019/07/07.jl
@@ -6,7 +6,7 @@ using Combinatorics
 function run_amps(m, init)
 	n = length(init)
 	cs = [Channel{Int}(Inf) for i=1:n]
-	tasks = [@async(IntCodeVM.exec_intcode!(copy(m), cs[i], cs[rem(i,n)+1])) for i=1:n]
+	tasks = [@async(exec_intcode!(copy(m), cs[i], cs[rem(i,n)+1])) for i=1:n]
 	put!.(cs, init) # initialize with respective amp init
 	put!(cs[1], 0)  # push 0 to first amp input
 	wait.(tasks)    # wait for all tasks to complete
