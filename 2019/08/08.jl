ca235c5c54162b1aecb538fc7d93aa0dfcc050e3
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-08 13:01:29 -0800

    slightly cleanup for prob1

--- 2019/08/08.jl
+++ 2019/08/08.jl
@@ -1,8 +1,8 @@
 img = reshape(parse.(Int, split(readlines()[1], "")), 25, 6, :)
 
-layer = argmin(reshape(sum(img .== 0, dims=[1,2]),:))
+layer = argmin(sum(img .== 0, dims=[1,2])[3])
 println(prod(count.(.==([1, 2]), [img[:,:,layer]])))            
 
 output = transpose(img[findmax(img .!= 2, dims=3)[2]][:,:,1])
-println.(mapslices(x -> join([" ", "#"][(x.==1).+1]), output, dims=2))
+println.(mapslices(join, replace(output, 0 => " ", 1 => "#"), dims=2))
 
