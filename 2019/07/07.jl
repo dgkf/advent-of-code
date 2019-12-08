6ed47893e13bbf19c7451982205e010d7f998c2c
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-08 14:58:34 -0800

    moved from Channels wait to @sync

--- 2019/07/07.jl
+++ 2019/07/07.jl
@@ -5,15 +5,15 @@ using Combinatorics
 # program execution with multiple processor feedback loop
 function run_amps(m, init)
     n = length(init)
-    cs = [Channel{Int}(Inf) for i=1:n]
-    tasks = [@async(exec_intcode!(copy(m), cs[i], cs[rem(i,n)+1])) for i=1:n]
-    put!.(cs, init) # initialize with respective amp init
-    put!(cs[1], 0)  # push 0 to first amp input
-    wait.(tasks)    # wait for all tasks to complete
+    cs = [Channel{Int}(Inf) for i=1:init]
+    push!.(cs, init)
+    push!(cs[1], 0)
+    @sync [@async(exec_intcode!(copy(m), cs[i], cs[rem(i,n)+1])) for i=1:n]
     take!(cs[1])
 end
 
 code = intcode(stdin)
+
 println(maximum(run_amps.([code], permutations(0:4))))
 println(maximum(run_amps.([code], permutations(5:9))))
 
