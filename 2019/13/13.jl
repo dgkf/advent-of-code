45361a1f0558c3e9afe0f886e5ab593d9e9ad41d
dgkf <18220321+dgkf@users.noreply.github.com>
2019-12-13 18:51:16 -0800

    cleaned up rendering

--- 2019/13/13.jl
+++ 2019/13/13.jl
@@ -2,6 +2,7 @@ include("../IntCodeVM.jl")
 using .IntCodeVM
 using OffsetArrays
 
+
 memory = intcode()
 
 # part 1
@@ -17,10 +18,12 @@ display = OffsetArray(zeros(Int8, display_dim...), -1, -1)
 memory_w_quarters = copy(memory)
 memory_w_quarters[0] = 2
 cpu = IntComp(IntCompState(memory_w_quarters))
+wait(cpu.state.output)
 
-function render_display(display, x)
-    println("\33[2J") # clear console
-    print("SCORE: "); println(score)
+function render_display(display, x, score)
+    # println("\33[2J") # clear console
+    run(Sys.iswindows() ? `cmd /c cls` : `clear` )
+    println(); print("SCORE: "); println(score)
 
     for row=eachrow(x)
         if row[1] == -1; continue; end
@@ -33,17 +36,18 @@ function render_display(display, x)
         2 => "B",
         3 => "=",
         4 => "O"))), "\n"))
-    println(join(div.([(1:size(display, 1) .- 1)...], 10)))
-    println(join([(1:size(display, 1) .- 1)...] .% 10))
+    println(join(div.([(0:size(display, 1))...], 10)))
+    println(join([(0:size(display, 1))...] .% 10))
 
     flush(stdout)
 end
 
 score = 0
+render = length(ARGS) > 0 && ARGS[1] .== "--show"
 paddle_loc = [0 0]
 ball_loc = [0 0]
 ball_vel = [0 0]
-while running(cpu) 
+while length(cpu.state.output.data) > 0
     # read out all buffered output
     output = reshape([popfirst!(cpu.state.output) for i=1:length(cpu.state.output.data)], 3, :)'
 
@@ -59,10 +63,13 @@ while running(cpu)
         global ball_loc = new_ball_loc
     end   
     
-    # render_display(display, output, score)
-
     ball_proj = ball_loc .+ ball_vel
     push!(cpu.state.input, cmp(ball_proj[2] >= paddle_loc[2] ? ball_loc[1] : ball_proj[1], paddle_loc[1]))
+    
+    if render; render_display(display, output, score); end
+    sleep(1/60)
+    
+    if !running(cpu.state); break; end
     wait(cpu.state.output)
 end
 
