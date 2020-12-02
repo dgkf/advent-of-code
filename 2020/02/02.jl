0250a8c3980e16929cbd47c2c5519762cb7ed17d
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-01 22:41:19 -0800

    cleanup!

--- 2020/02/02.jl
+++ 2020/02/02.jl
@@ -1,16 +1,20 @@
 raw_input = readlines()
 
+struct PasswordLog
+    min::Int
+    max::Int
+    l::Char
+    pw::AbstractString
+end
+
 log_regex = r"(?<min>\d+)-(?<max>\d+) (?<l>\w): (?<pw>\w+)"
 input = map(match.([log_regex], raw_input)) do i
-    Dict(:min => parse(Int, i[:min]),
-         :max => parse(Int, i[:max]),
-         :l   => i[:l][1],
-         :pw  => i[:pw])
+    PasswordLog(parse(Int, i[:min]), parse(Int, i[:max]), i[:l][1], i[:pw])
 end
 
 # part 1
-println(sum(i[:min] <= count(==(i[:l]), i[:pw]) <= i[:max] for i = input))
+println(sum(i.min <= count(==(i.l), i.pw) <= i.max for i = input))
 
 # part 2
-println(sum(xor(i[:pw][i[:min]] == i[:l], i[:pw][i[:max]] == i[:l]) for i in input))
+println(sum(xor(i.pw[i.min] == i.l, i.pw[i.max] == i.l) for i in input))
 
