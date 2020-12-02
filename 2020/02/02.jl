384849ec21afde7164f93b121c5ef192de268379
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-02 07:58:37 -0800

    cleanup!

--- 2020/02/02.jl
+++ 2020/02/02.jl
@@ -13,7 +13,7 @@ input = map(match.([log_regex], raw_input)) do i
 end
 
 # part 1
-println(sum(i.min ≤ count(==(i.l), i.pw) ≥ i.max for i = input))
+println(sum(i.min ≤ count(==(i.l), i.pw) ≤ i.max for i = input))
 
 # part 2
 println(sum(xor(i.pw[i.min] == i.l, i.pw[i.max] == i.l) for i = input))
