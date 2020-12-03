b1547a46915cbe67c737d8499606811aa467b648
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-03 09:46:33 -0800

    cleanup!

--- 2020/03/03.jl
+++ 2020/03/03.jl
@@ -1,20 +1,15 @@
 input = readlines()
-trees = reshape([i == '#' for i=join(input)], :, length(input))
+trees = reshape([i == '#' for i = join(input)], :, length(input))
+tx, ty = size(trees)
 
 dirs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 n_trees = [0, 0, 0, 0, 0]
 
 for (i, d) = enumerate(dirs)
-    coords = [1, 1]
-    while coords[2] <= size(trees)[2]
-        n_trees[i] += trees[coords[1], coords[2]]
-        coords = [mod1(coords[1] + d[1], size(trees)[1]), coords[2] + d[2]]
-    end
+    xs = mod1.(range(1; length = ty ÷ d[2], step = d[1]), tx)
+    ys = range(1; length = ty ÷ d[2], step = d[2])
+    n_trees[i] = sum(trees[CartesianIndex.(xs, ys)])
 end
 
 println(n_trees[2])
-print(prod(n_trees))
-
-
-
-
+println(prod(n_trees))
