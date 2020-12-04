03c77101de0d4401ce87d467514239e1241711ce
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-03 23:24:56 -0800

    cleanup!

--- 2020/04/04.jl
+++ 2020/04/04.jl
@@ -1,35 +1,26 @@
-input = split.(replace.(split(join(readlines(), "\n"), "\n\n"), "\n" => " "))
-passports = map(input) do passport
-    p = split.(passport, ":")
-    Dict(first.(p) .=> last.(p))
-end
+input = split.(split(join(readlines(), "\n"), "\n\n"), r"\s+")
+passports = map(passport -> Dict(split.(passport, ":")), input)
 
 reqids = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-n_valid = [0, 0]
 
-for p = passports
-    # part 1
-    n_valid[1] += all(reqids .∈ [keys(p)])
+# part 1
+count(passports) do p
+    all(in(keys(p)), reqids)
+end |> println
 
-    # part 2
-    if all(reqids .∈ [keys(p)]) &&
-        1920 <= parse(Int, get(p, "byr", -1)) <= 2002 &&
-        2010 <= parse(Int, get(p, "iyr", -1)) <= 2020 &&
-        2020 <= parse(Int, get(p, "eyr", -1)) <= 2030 &&
-        match(r"^#[0-9a-f]{6}$", get(p, "hcl", "")) != nothing &&
-        get(p, "ecl", "") ∈ ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] &&
-        match(r"^\d{9}$", get(p, "pid", 0)) != nothing
+# part 2
+count(passports) do p
+    hgt = match(r"^(\d+)(cm|in)$", get(p, "hgt", ""))
 
-        hgt = match(r"^(\d+)(cm|in)$", get(p, "hgt", ""))
-
-        if hgt != nothing && (
-                (hgt[2] == "cm" && (150 <= parse(Int, hgt[1]) <= 193)) ||
-                (hgt[2] == "in" && (59 <= parse(Int, hgt[1]) <= 76)))
-
-            n_valid[2] += 1
-        end
-    end
-end
-    
-println.(n_valid)
-
+    all(in(keys(p)), reqids) &&
+    1920 <= parse(Int, get(p, "byr", 0)) <= 2002 &&
+    2010 <= parse(Int, get(p, "iyr", 0)) <= 2020 &&
+    2020 <= parse(Int, get(p, "eyr", 0)) <= 2030 &&
+    occursin(r"^#[0-9a-f]{6}$", get(p, "hcl", "")) &&
+    get(p, "ecl", "") ∈ ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] &&
+    occursin(r"^\d{9}$", get(p, "pid", 0)) && 
+    hgt isa RegexMatch && (
+        (hgt[2] == "cm" && (150 <= parse(Int, hgt[1]) <= 193)) ||
+        (hgt[2] == "in" && (59 <= parse(Int, hgt[1]) <= 76))
+    )
+end |> println
