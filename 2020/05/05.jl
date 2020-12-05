270a09b12163eb894124101f2df74e041e1f2b6e
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-04 22:45:25 -0800

    cleanup!

--- 2020/05/05.jl
+++ 2020/05/05.jl
@@ -1,10 +1,12 @@
-input = readlines("./utils/cache/2020/5/input.txt")
+input = readlines("utils/cache/2020/5/input.txt")
 seat_ids = map(input) do i
     i = split(i, "") .∈ [["B", "R"]]
-    8 * sum(i[1:7] .* (2 .^ (6:-1:0))) + sum(i[8:10] .* (2 .^ (2:-1:0)))
+    sum(i .* (2 .^ (9:-1:0)))
 end
 
+# part 1
 println(maximum(seat_ids))
 
-all_seat_ids = collect(minimum(seat_ids):maximum(seat_ids))
-println(all_seat_ids[findfirst(all_seat_ids .∉ [seat_ids])])
+# part 2
+all_ids = UnitRange(extrema(seat_ids)...)
+println(all_ids[findfirst(i -> i ∉ seat_ids, all_ids)])
