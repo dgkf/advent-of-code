3f475863094cf4dfa9c7541eca965d1701935e26
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-05 22:20:43 -0800

    cleanup!

--- 2020/06/06.jl
+++ 2020/06/06.jl
@@ -4,8 +4,8 @@ input = read(stdin, String)
 groups = split.(split(input, "\n\n"), "\n")
 
 # part 1
-sum(length(union(Set.(group)...)) for group in groups) |> println
+sum(length(union(group...)) for group in groups) |> println
 
 # part 2
-sum(length(intersect(Set.(group)...)) for group in groups) |> println
+sum(length(intersect(group...)) for group in groups) |> println
 
