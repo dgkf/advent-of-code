0d45c2cb47de91b50bc3b3a2819e8c2e51b3b563
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-05 21:57:21 -0800

    cleanup!

--- 2020/06/06.jl
+++ 2020/06/06.jl
@@ -1,11 +1,11 @@
-input = readlines()
+input = read(stdin, String)
 
 # create array of groups (arrays of strings)
-groups = split.(split(join(input, "\n"), "\n\n"), "\n")
+groups = split.(split(input, "\n\n"), "\n")
 
 # part 1
-sum(length(union(Set.(group)...)) for group in groups)
+sum(length(union(Set.(group)...)) for group in groups) |> println
 
 # part 2
-sum(length(intersect(Set.(group)...)) for group in groups)
+sum(length(intersect(Set.(group)...)) for group in groups) |> println
 
