2dae0cd6c6b9af7fd4691559d6bebbf562d75f5d
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-10 11:16:09 -0800

    cleaning up and adding splitmap

--- 2020/10/10.jl
+++ 2020/10/10.jl
@@ -6,40 +6,19 @@ d = diff(sort(input))
 sum(d .== 1) * sum(d .== 3)
 
 # part 2
-# figure out how many sequential 1's we have
-max_seq_ones = 0
-n = 0
-for i = diff(sort(input))
-    n = i == 1 ? n += 1 : 0
-    max_seq_ones = max(max_seq_ones, n)
+function count_paths(x)
+    length(x) == 1 && return 1
+    sum(count_paths(x[findfirst(==(x[1]+j), x):end]) for j = 1:3 if (x[1]+j) ∈ x)
 end
 
-function count_paths(input)
-    n = 0
-    length(input) == 1 && return 1
-    for j = 1:3
-        if (input[1] + j) ∈ input
-            n += count_paths(input[findfirst(==(input[1] + j), input):end])
-        end
-    end
-    return n
+splitmap(f::Function, x; keep = true) =  splitmap(f.(x), x; keep = keep)
+function splitmap(s::AbstractArray{Bool}, x; keep = true)
+    split_idxs = findall(s)
+    split_starts = vcat([1], split_idxs .+ (keep ? 0 : 1))
+    split_ends = vcat(split_idxs, [length(x)+1])
+    [x[i:j-1] for (i,j) = zip(split_starts, split_ends)]
 end
 
-# count number of paths given a sequence of n ones
-n_seqs = repeat([0], max_seq_ones)
-for seq = 1:max_seq_ones
-    n_seqs[seq] = count_paths(1:seq+1)
-end
-
-# count sequential 1s and multiply by possible number of branching paths
-n_paths = 1
-n = 0
-for i = diff(sort(input))
-    if i == 1
-        n += 1
-    elseif n > 0
-        n_paths *= n_seqs[n]
-        n = 0
-    end
-end
+sortinput = sort(input)
+prod(map(count_paths, splitmap(vcat([false], diff(sortinput) .== 3), sortinput)))
 
