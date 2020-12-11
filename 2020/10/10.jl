2fbdb9fef7cf4d6e5f3b330c849b00031b075130
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-11 11:21:13 -0800

    using @time instead of @btime

--- 2020/10/10.jl
+++ 2020/10/10.jl
@@ -1,4 +1,3 @@
-using BenchmarkTools
 using Memoize
 using Memoization
 
@@ -6,8 +5,8 @@ input = sort(parse.(Int, readlines("utils/cache/2020/10/input.txt")))
 input = vcat([0], input, [maximum(input)+3])
 
 # part 1
-d = diff(sort(input))
-println(sum(d .== 1) * sum(d .== 3))
+Δinput = diff(input)
+println(sum(Δinput .== 1) * sum(Δinput .== 3))
 
 # part 2: solving for brain teaser trick
 function count_paths(x)
@@ -27,7 +26,7 @@ function splitmap(s::AbstractArray{Bool}, x; keep = true)
     splitmap(cumsum(s), x; keep = keep) 
 end
 
-println(@btime prod(map(count_paths, splitmap(vcat(false, diff(input) .== 3), input))))
+println(@time prod(map(count_paths, splitmap(vcat(false, diff(input) .== 3), input))))
 
 # part 2: solving by memoising and brute force
 const trailing_x = Dict{Array{Int}, Int}()
@@ -45,7 +44,7 @@ function count_paths_memoize(x)
     return n
 end
 
-println(@btime count_paths_memoize(input))
+println(@time count_paths_memoize(input))
 
 # part 2: solving using Memoize.jl
 Memoize.@memoize Dict function count_paths_memoizejl(x)
@@ -53,7 +52,7 @@ Memoize.@memoize Dict function count_paths_memoizejl(x)
     sum(count_paths_memoizejl(x[findfirst(==(j), x):end]) for j = x[1] .+ (1:3) if j ∈ x)
 end
 
-println(@btime count_paths_memoizejl(input))
+println(@time count_paths_memoizejl(input))
 
 # part 2: solving using Memoization.jl
 Memoization.@memoize Dict function count_paths_memoizationjl(x)
@@ -61,5 +60,5 @@ Memoization.@memoize Dict function count_paths_memoizationjl(x)
     sum(count_paths_memoizationjl(x[findfirst(==(j), x):end]) for j = x[1] .+ (1:3) if j ∈ x)
 end
 
-println(@btime count_paths_memoizationjl(input))
+println(@time count_paths_memoizationjl(input))
 
