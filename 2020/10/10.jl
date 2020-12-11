ea83c7602420d453842ee8b13d39d67829c3e12e
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-10 17:48:51 -0800

    adding memoised solution for day 10

--- 2020/10/10.jl
+++ 2020/10/10.jl
@@ -1,26 +1,45 @@
-input = parse.(Int, readlines())
+input = sort(parse.(Int, readlines()))
 input = vcat([0], input, [maximum(input)+3])
 
 # part 1
 d = diff(sort(input))
-sum(d .== 1) * sum(d .== 3)
+println(sum(d .== 1) * sum(d .== 3))
 
-# part 2
+# part 2: solving for brain teaser trick
 function count_paths(x)
     length(x) == 1 && return 1
     sum(count_paths(x[findfirst(==(j), x):end]) for j = x[1] .+ (1:3) if j ∈ x)
 end
 
-splitmap(f::Function, x; kwargs...) = splitmap(f.(x), x; kwargs...)
+splitmap(f::Function, x; kwargs...) = 
+    splitmap(f.(x), x; kwargs...)
+
+splitmap(s::AbstractArray{Int}, x; kwargs...) = 
+    [x[s .== i] for i = unique(skipmissing(s))]
+
 function splitmap(s::AbstractArray{Bool}, x; keep = true)
     groups = cumsum(s)
-    !keep && groups[s] = missing 
-    splitmap(cumsum(s), x; kwargs...) 
-end
-function splitmap(s::AbstractArray{Int}, x; keep = true)
-    [x[s .== i] for i = unique(s)]
+    !keep && (groups[s] = missing)
+    splitmap(cumsum(s), x; keep = keep) 
 end
 
-sortinput = sort(input)
-prod(map(count_paths, splitmap(vcat(false, diff(sortinput) .== 3), sortinput)))
+println(prod(map(count_paths, splitmap(vcat(false, diff(input) .== 3), input))))
+
+# part 2: solving by memoising and brute force
+const trailing_x = Dict{Array{Int}, Int}()
+function count_paths(x)
+    length(x) == 1 && return 1
+    n = 0
+    for j = x[1] .+ (1:3)
+        if j ∈ x
+            trailing_xi = x[findfirst(==(j), x):end]
+            n += get!(trailing_x, trailing_xi) do
+                count_paths(trailing_xi)
+            end
+        end
+    end
+    return n
+end
+
+println(count_paths(input))
 
