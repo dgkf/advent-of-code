9f42006c276a24c9b058477461181f181f7f5bd1
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-13 00:11:20 -0800

    cleanup!

--- 2020/13/13.jl
+++ 2020/13/13.jl
@@ -1,18 +1,23 @@
-input = readlines("utils/cache/2020/13/test01.txt")
+input = readlines()
+
 n = parse(Int, input[1])
-buses = parse.(Int, replace(split(input[2], ","), "x" => "-1"))
+schedule = parse.(Int, replace(split(input[2], ","), "x" => "-1"))
+
+bus_n = filter(!=(-1), schedule)
+bus_dt = (1:length(schedule))[schedule .!= -1] .- 1
 
 # part 1
-buses_nums = filter(!=(-1), buses)
-rems = buses_nums .- n .% buses_nums
-buses_nums[findmin(rems)[2]] * findmin(rems)[1] 
+min_remainder = findmin(bus_n .- n .% bus_n)
+println(bus_n[min_remainder[2]] * min_remainder[1])
 
 # part 2
-buses_offsets = (1:length(buses))[buses .!= -1] .- 1
+inc = bus_n[1] 
+n = 0
+for (i, offset) in zip(bus_n[2:end], bus_dt[2:end])
+    while (n + offset) % i != 0
+        n += inc
+    end
+    inc = lcm(inc, i)
+end
+println(n)
 
-
-# ex
-n = 1068781
-prod((((buses_nums .- buses_offsets) .% buses_nums) .* buses_nums)[2:end])
-
-1e12 / 50
