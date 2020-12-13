cc3674dfefd621f308d9f549443ce84d08a9d610
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-13 00:15:33 -0800

    making it executable

--- 2020/13/13.jl
+++ 2020/13/13.jl
@@ -7,17 +7,21 @@ bus_n = filter(!=(-1), schedule)
 bus_dt = (1:length(schedule))[schedule .!= -1] .- 1
 
 # part 1
-min_remainder = findmin(bus_n .- n .% bus_n)
-println(bus_n[min_remainder[2]] * min_remainder[1])
+min_rem_i, min_rem = findmin(bus_n .- n .% bus_n)
+println(bus_n[min_rem_i] * min_rem)
 
 # part 2
-inc = bus_n[1] 
-n = 0
-for (i, offset) in zip(bus_n[2:end], bus_dt[2:end])
-    while (n + offset) % i != 0
-        n += inc
+function find_seq_bus_arrivals(bus_n, bus_dt)
+    inc = bus_n[1] 
+    n = 0
+    for (i, offset) in zip(bus_n[2:end], bus_dt[2:end])
+        while (n + offset) % i != 0
+            n += inc
+        end
+        inc = lcm(inc, i)
     end
-    inc = lcm(inc, i)
+    return n
 end
-println(n)
+
+println(find_seq_bus_arrivals(bus_n, bus_dt))
 
