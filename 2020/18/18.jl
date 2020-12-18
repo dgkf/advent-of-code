75dccf5e9b32c0957aed236c290ec35ece9838e6
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-17 22:24:59 -0800

    revert back to first style

--- 2020/18/18.jl
+++ 2020/18/18.jl
@@ -1,10 +1,7 @@
 input = readlines()
 
-eval_replace(s, x::Pair...) = 
-    eval(Meta.parse(reduce((l, r) -> replace(l, r), x; init = s)))
-
 ⨦(a,b) = a * b  # define "multiplication" with same precedence as "+"
-println(sum(map(l -> eval_replace(l, "*" => "⨦"), input)))
+println(sum(map(l -> eval(Meta.parse(replace(l, "*" => "⨦"))), input)))
 
 ⨱(a,b) = a + b  # define "addition" with precedence of "*"
-println(sum(map(l -> eval_replace(l, "*" => "⨦", "+" => "⨱"), input)))
+println(sum(map(l -> eval(Meta.parse(replace(replace(l, "*" => "⨦"), "+" => "⨱"))), input)))
