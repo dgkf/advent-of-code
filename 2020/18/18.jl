1e8edbc08b68e5d120875a7dff5e8c4e3aa2e956
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-17 22:41:40 -0800

    adding suggestion from u/Seelengrab on reddit

--- 2020/18/18.jl
+++ 2020/18/18.jl
@@ -1,7 +1,7 @@
 input = readlines()
 
 ⨦(a,b) = a * b  # define "multiplication" with same precedence as "+"
-println(sum(map(l -> eval(Meta.parse(replace(l, "*" => "⨦"))), input)))
+println(sum(l -> eval(Meta.parse(replace(l, "*" => "⨦"))), input))
 
 ⨱(a,b) = a + b  # define "addition" with precedence of "*"
-println(sum(map(l -> eval(Meta.parse(replace(replace(l, "*" => "⨦"), "+" => "⨱"))), input)))
+println(sum(l -> eval(Meta.parse(replace(replace(l, "*" => "⨦"), "+" => "⨱"))), input))
