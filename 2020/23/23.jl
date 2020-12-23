dce0794ad6d70dba4903be0b59496922deeb2dd0
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-23 11:45:52 -0800

    a more respectable approach

--- 2020/23/23.jl
+++ 2020/23/23.jl
@@ -1,45 +1,58 @@
 cups = parse.(Int, split(readlines(stdin)[1], ""))
 
-function cup_game(cups, n = 100)
-    cups = deepcopy(cups)
-    cupsinto = deepcopy(cups)
+prev_cup(cups, i) = first(cups[i])
+next_cup(cups, i) = last(cups[i])
+function cups_game(cups, n = 100)
+    current = cups[1]
     maxcup = maximum(cups)
-    i = 1
-    while i <= n
-        i % max(n ÷ 1000, 1) == 0 && (println("$(i / n)%"))
 
-        dest = cups[1] - 1
-        dest ∈ cups[2:4] && (dest = dest - 1)
-        dest ∈ cups[2:4] && (dest = dest - 1)
-        dest ∈ cups[2:4] && (dest = dest - 1)
+    cups = Dict{Int,Tuple{Int,Int}}(i => (prev, next) for (i, prev, next) in 
+        zip(cups, cups[mod1.(0:end-1, end)], cups[mod1.(2:end+1, end)]))
 
-        dest < 1 && (dest = maxcup)
-        dest ∈ cups[2:4] && (dest = dest - 1)
-        dest ∈ cups[2:4] && (dest = dest - 1)
-        dest ∈ cups[2:4] && (dest = dest - 1)
+    for i in 1:n
+        next1 = next_cup(cups, current)        
+        next2 = next_cup(cups, next1)
+        next3 = next_cup(cups, next2)
+        next4 = next_cup(cups, next3)
+        nextset = (next1, next2, next3)
 
-        loc = findfirst(==(dest), cups)
+        dest = current - 1
+        dest in nextset && (dest -= 1)
+        dest in nextset && (dest -= 1)
+        dest in nextset && (dest -= 1)
+        dest < 1        && (dest = maxcup)
+        dest in nextset && (dest -= 1)
+        dest in nextset && (dest -= 1)
+        dest in nextset && (dest -= 1)
 
-        cupsinto[1:loc-4] .= @view cups[5:loc]
-        cupsinto[loc-3:loc-1] .= @view cups[2:4]
-        cupsinto[loc:end-1] .= @view cups[loc+1:end]
-        cupsinto[end:end] .= @view cups[1:1]
-        cups, cupsinto = cupsinto, cups
+        cups[current] = (cups[current][1], cups[next3][2])
+        cups[next4]   = (current, cups[next4][2])
+        cups[next3]   = (cups[next3][1], cups[dest][2])
+        cups[dest]    = (cups[dest][1], next1)
+        cups[next1]   = (dest, cups[next1][2])
 
-        i += 1
+        current = next4
     end
+
     return cups
 end
 
+function get_n_cups(cups, start, n = length(cups))
+    out = Array{Int}([])
+    i = start
+    for _ = 1:n
+        push!(out, i)
+        i = next_cup(cups, i)
+    end
+    return out
+end
 
 # part 1
-cupsout = cup_game(cups)
-loc1 = findfirst(==(1), cupsout)
-println(join(string.([cupsout[loc1+1:end]; cupsout[1:loc1-1]]), ""))
+cupsout = cups_game(cups, 100)
+println(join(string.(get_n_cups(cupsout, 1)[2:end]), ""))
 
-# part 2: brute force, smdh
-cups = Array{Int}([cups; 10:1e6])
-cupsout = cup_game(cups, 1e7)
-loc1 = findfirst(==(1), cupsout)
-cupsout[loc1+1] * cupsout[loc1+2]
+# part 2
+cupslong = Array{Int}([cups; 10:1e6])
+cupsout  = cups_game(cupslong, 1e7)
+println(prod(get_n_cups(cupsout, 1, 3)[2:end]))
 
