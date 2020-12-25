62415bac0817d96e87b3beea7023e3fcbe7e7cf6
dgkf <18220321+dgkf@users.noreply.github.com>
2020-12-25 11:52:25 -0800

    reworked using channels

--- 2020/25/25.jl
+++ 2020/25/25.jl
@@ -1,29 +1,14 @@
-key1, key2 = parse.(Int, readlines(stdin))
+key1, key2 = parse.(Int, readlines("utils/cache/2020/25/input.txt"))
 
-function transform_subject_number(x = 1, subjnum = 7)
-    x = (x * subjnum) % 20201227
+function transform_subject_number(subjnum = 7)
+    Channel{Int}(1) do c while true
+        x = 1
+        while true
+            x = (x * subjnum) % 20201227
+            push!(c, x)
+        end
+    end
 end
 
-val1 = 1
-nloops1 = 0
-while key1 != val1
-    nloops1 += 1
-    val1 = transform_subject_number(val1)
-end
-
-
-val2 = 1
-nloops2 = 0
-while key2 != val2
-    nloops2 += 1
-    val2 = transform_subject_number(val2)
-end
-
-
-x = 1
-for i = 1:nloops2
-    x = transform_subject_number(x, key1)
-end
-
-println(x)
-
+nloops1 = first(i for (i, v) = enumerate(transform_subject_number()) if v == key1)
+println(first(v for (i, v) = enumerate(transform_subject_number(key2)) if i == nloops1))
