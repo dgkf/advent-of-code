da007d742f9f1a20e9adceecc9815b7998940660
dgkf <18220321+dgkf@users.noreply.github.com>
2021-12-01 22:13:37 -0800

    adding solution for 2021/02;rust

---
+++ 2021/02/rust/src/main.rs
@@ -0,0 +1,84 @@
+use std::io::BufRead;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Up,
+    Forward,
+    Down,
+    Backward
+}
+
+#[derive(Debug, PartialEq)]
+struct Movement {
+    dir: Direction,
+    x: i32
+}
+
+impl FromStr for Movement {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut strs = line.split(" ");
+        let dir: Direction = match strs.next() {
+            Some("up")       => Some(Direction::Up),
+            Some("forward")  => Some(Direction::Forward),
+            Some("down")     => Some(Direction::Down),
+            Some("backward") => Some(Direction::Backward),
+            _                => None
+        }.unwrap();
+
+        let x: i32 = strs.next().unwrap().parse().unwrap();
+        Ok(Movement { dir, x })
+    }
+}
+
+fn parse_input() -> Vec<Movement> {
+    std::io::stdin()
+        .lock()
+        .lines()
+        .filter_map(|l| Some(Movement::from_str(&l.unwrap()).unwrap()))
+        .collect()
+}
+
+fn apply_movements(moves: &Vec<Movement>) -> Vec<i32> {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+
+    for m in moves {
+        match m.dir {
+            Direction::Forward  => x += m.x,
+            Direction::Backward => x -= m.x,
+            Direction::Up       => y += m.x,
+            Direction::Down     => y -= m.x,
+        };
+    };
+
+    vec![x, y]
+}
+
+fn apply_movements_with_aim(moves: &Vec<Movement>) -> Vec<i32> {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut aim: i32 = 0;
+
+    for m in moves {
+        match m.dir {
+            Direction::Forward  => { x += m.x; y -= aim * m.x },
+            Direction::Backward => { x -= m.x; y += aim * m.x },
+            Direction::Up       => aim -= m.x,
+            Direction::Down     => aim += m.x,
+        };
+    };
+
+    vec![x, y]
+}
+
+fn main() {
+    let data = parse_input();
+    let pos = apply_movements(&data);
+    let pos_w_aim = apply_movements_with_aim(&data);
+    println!("{:?}", pos[0] * -pos[1]);
+    println!("{:?}", pos_w_aim[0] * -pos_w_aim[1]);
+}
+
