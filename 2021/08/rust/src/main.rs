29b734014da6d4e5b3c669c6e4b2c0009cb23b86
dgkf <18220321+dgkf@users.noreply.github.com>
2021-12-10 15:53:19 -0800

    cleaning up data structures & data flow

--- 2021/08/rust/src/main.rs
+++ 2021/08/rust/src/main.rs
@@ -1,137 +1,139 @@
 use std::io::BufRead;
 use std::time::Instant;
-use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
+struct SevenPanelDisplay {
+    panels: [bool; 7],
+}
+
+impl FromStr for SevenPanelDisplay {
+    type Err = std::string::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const LETTERS: [char; 7] = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+        let mut panels: [bool; 7] = [false; 7];
+        for i in 0..panels.len() { panels[i] = s.contains(LETTERS[i]) }
+        Ok(SevenPanelDisplay{ panels })
+    }
+}
 
 #[derive(Debug, PartialEq)]
 struct Entry {
-    signals: Vec<Vec<u8>>,
-    output: Vec<Vec<u8>>
+    output: Vec<u8>,
 }
 
-fn build_panel_counts_mapping() -> HashMap<u8, u8> {
-    let mut panel_counts = HashMap::new();
-    panel_counts.insert(6, 1);
-    panel_counts.insert(4, 4);
-    panel_counts.insert(9, 5);
-    panel_counts
+impl FromStr for Entry {
+    type Err = std::string::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut entryline = s.split(" | ");
+
+        let mut signals: [SevenPanelDisplay; 10] = [SevenPanelDisplay{ panels: [false; 7] }; 10];
+        for (i, s) in entryline.next().unwrap().split(" ").enumerate() {
+            signals[i] = SevenPanelDisplay::from_str(s).unwrap();
+        }
+
+        let panel_map = remap_signals(&mut signals);
+        let output = entryline.next().unwrap().split(" ")
+            .map(|s| SevenPanelDisplay::from_str(s).unwrap())
+            .map(|s| panel_to_value(&remap_signal(&s, &panel_map)))
+            .collect();
+
+        Ok(Entry{ output })
+    }
 }
 
-fn build_digit_array_mapping() -> HashMap<[u8; 7], u8> {
-    let mut digits = HashMap::new();
-    digits.insert([1, 1, 1, 0, 1, 1, 1], 0);
-    digits.insert([0, 0, 1, 0, 0, 1, 0], 1);
-    digits.insert([1, 0, 1, 1, 1, 0, 1], 2);
-    digits.insert([1, 0, 1, 1, 0, 1, 1], 3);
-    digits.insert([0, 1, 1, 1, 0, 1, 0], 4);
-    digits.insert([1, 1, 0, 1, 0, 1, 1], 5);
-    digits.insert([1, 1, 0, 1, 1, 1, 1], 6);
-    digits.insert([1, 0, 1, 0, 0, 1, 0], 7);
-    digits.insert([1, 1, 1, 1, 1, 1, 1], 8);
-    digits.insert([1, 1, 1, 1, 0, 1, 1], 9);
-    digits
-}
-
-fn signal_to_vec(signal: String) -> Vec<u8> {
-    signal.chars().map(|c| (c as u8) - 97).collect()    
-}
-
-fn parse_line(line: String) -> Entry {
-    let mut entryline = line.split(" | ");
-    let signals = entryline.next().unwrap().split(" ").map(|s| signal_to_vec(s.to_string())).collect();
-    let output  = entryline.next().unwrap().split(" ").map(|s| signal_to_vec(s.to_string())).collect();
-    Entry{ signals: signals, output: output }
+fn panel_to_value(s: &SevenPanelDisplay) -> u8 {
+    match s.panels {
+        [ true,  true,  true, false,  true,  true,  true] => 0,
+        [false, false,  true, false, false,  true, false] => 1,
+        [ true, false,  true,  true,  true, false,  true] => 2,
+        [ true, false,  true,  true, false,  true,  true] => 3,
+        [false,  true,  true,  true, false,  true, false] => 4,
+        [ true,  true, false,  true, false,  true,  true] => 5,
+        [ true,  true, false,  true,  true,  true,  true] => 6,
+        [ true, false,  true, false, false,  true, false] => 7,
+        [ true,  true,  true,  true,  true,  true,  true] => 8,
+        [ true,  true,  true,  true, false,  true,  true] => 9,
+        _                                                 => 0,
+    }
 }
 
 fn parse_input() -> Vec<Entry> {
     std::io::stdin()
         .lock()
         .lines()
-        .map(|line| parse_line(line.unwrap()))
+        .map(|line| Entry::from_str(&line.unwrap()).unwrap())
         .collect()
 }
 
-fn remap_signal(s: &Vec<u8>, map: &Vec<u8>) -> Vec<u8> {
-    s.iter().map(|i| map[*i as usize]).collect()
+fn remap_signal(s: &SevenPanelDisplay, map: &Vec<usize>) -> SevenPanelDisplay {
+    let mut panels = [false; 7];
+    for i in 0..s.panels.len() {
+        panels[i] = s.panels[map[i]];
+    }
+    SevenPanelDisplay{ panels }
 }
 
-fn remap_entry(ent: &Entry, pc: &HashMap<u8, u8>) -> Entry {
+fn remap_signals(signals: &mut [SevenPanelDisplay; 10]) -> Vec<usize> {
     let mut panel_counts = vec![0; 7];
-    let mut panel_map = vec![0; 7];
+    let mut panel_map: Vec<usize> = vec![0; 7];
+
+    let signal_of_len = |s: [SevenPanelDisplay; 10], n| s.into_iter()
+        .filter(|s| s.panels.iter().filter(|i| **i).count() == n)
+        .next()
+        .unwrap();
+
+    let signal_of_len_4 = signal_of_len(*signals, 4);
+    let signal_of_len_2 = signal_of_len(*signals, 2);
 
     // count occurences of each panel
-    for s in &ent.signals {
-        for i in s {
-            panel_counts[*i as usize] += 1;
+    for s in *signals {
+        for (i, b) in s.panels.iter().enumerate() {
+            if *b { panel_counts[i] += 1; }
         }
     }
 
-    // map unique panel counts
+    // map unique panel counts (rely primarily on panel counts, using 
+    // occurance in unambiguous signals when panel count is ambiguous)
     for (i, &c) in panel_counts.iter().enumerate() {
-        if c == 7 {
-            // if i in signal of length 4 => panel 3
-            // else                       => panel 6
-            if ent.signals.iter().find(|s| s.len() == 4).unwrap().contains(&(i as u8)) {
-                panel_map[i] = 3
-            } else {
-                panel_map[i] = 6
-            }
-        } else if c == 8 {
-            // if i in signal of length 2 => panel 2
-            // else                       => panel 0
-            if ent.signals.iter().find(|s| s.len() == 2).unwrap().contains(&(i as u8)) {
-                panel_map[i] = 2
-            } else {
-                panel_map[i] = 0
-            }
-        } else {
-            panel_map[i] = pc[&c]; 
-        }
+        let map_to = match c {
+            // if panel count of 7, use occurance in signal of len 4 to determine
+            7 => if signal_of_len_4.panels[i] { 3 } else { 6 },
+            // if panel count of 8, use occurance in signal of len 2 to determine
+            8 => if signal_of_len_2.panels[i] { 2 } else { 0 },
+            4 => 4,
+            6 => 1,
+            9 => 5,
+            _ => 0,
+        };
+
+        panel_map[map_to] = i;
     }
 
-    // build new entry with consistent numbering schemes
-    let signals: Vec<Vec<u8>> = ent.signals.iter().map(|s| remap_signal(s, &panel_map)).collect();
-    let output:  Vec<Vec<u8>> = ent.output.iter().map(|s| remap_signal(s, &panel_map)).collect();
-    Entry{ signals, output }
+    // build new display with consistent numbering schemes
+    for i in 0..signals.len() {
+        signals[i] = remap_signal(&signals[i], &panel_map)
+    }
+
+    panel_map
 }
 
-fn remap_entries(entries: &Vec<Entry>, pc: &HashMap<u8, u8>) -> Vec<Entry> {
-    entries.iter().map(|e| remap_entry(e, &pc)).collect()
-}
-
-fn output_vec_to_panel_vec(v: &Vec<u8>) -> [u8; 7] {
-    let mut out: [u8; 7] = [0; 7];
-    for i in v { out[*i as usize] = 1 }
-    out
-}
-
-fn entry_output(ent: &Entry, digits: &HashMap<[u8; 7], u8>) -> Vec<u8> {
-    ent.output.iter()
-        .map(|o| digits[&output_vec_to_panel_vec(o)])
-        .collect()
-}
-
-fn entry_outputs(entries: &Vec<Entry>, digits: &HashMap<[u8; 7], u8>) -> Vec<Vec<u8>> {
-    entries.iter().map(|e| entry_output(e, &digits)).collect()
-}
-
-fn count_easy(output: &Vec<Vec<u8>>) -> u32 {
+fn count_easy(entries: &Vec<Entry>) -> u32 {
     const EASY: [u8; 4] = [1, 4, 7, 8];
     let mut count = 0;
-    for output_i in output {
-        for n in output_i {
-            if EASY.contains(n) {
-                count += 1;
-            }
+    for e in entries {
+        for o in &e.output {
+            if EASY.contains(o) { count += 1 }
         }
     }
     count
 }
 
-fn sum_all(output: &Vec<Vec<u8>>) -> u32 {
-    let mut sum: u32 = 0;
-    for output_i in output {
-        for (i, n) in output_i.iter().rev().enumerate() {
-            sum += (*n as u32) * (10 as u32).pow(i as u32);
+fn sum_all(entries: &Vec<Entry>) -> u64 {
+    let mut sum: u64 = 0;
+    for e in entries {
+        for (i, o) in e.output.iter().rev().enumerate() {
+            sum += (*o as u64) * 10_u64.pow(i as u32);
         }
     }
     sum
@@ -140,10 +142,7 @@ fn sum_all(output: &Vec<Vec<u8>>) -> u32 {
 fn main() {
     let now = Instant::now();
     let input = parse_input();
-    let panel_counts = build_panel_counts_mapping();
-    let digits = build_digit_array_mapping();
-    let output = entry_outputs(&remap_entries(&input, &panel_counts), &digits);
-    println!("{:?}", count_easy(&output));
-    println!("{:?}", sum_all(&output));
+    println!("{:?}", count_easy(&input));
+    println!("{:?}", sum_all(&input));
     println!("Elapsed: {:?}", now.elapsed());
 }
