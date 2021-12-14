ad8f946b1677dac4fdd6afb246201778b7d4808a
dgkf <18220321+dgkf@users.noreply.github.com>
2021-12-14 10:11:38 -0800

    cleaning up solution for day 2021/14;rust

--- 2021/14/rust/src/main.rs
+++ 2021/14/rust/src/main.rs
@@ -61,40 +61,45 @@ fn n_steps_naive(t: &String, inserts: &HashMap<[char; 2], char>, n: usize) -> St
 
 fn n_steps(t: &String, pairs: &HashMap<[char; 2], [[char; 2]; 2]>, n: usize) -> u64 {
     // count pairs in t
-    let mut counter: HashMap<[char; 2], u64> = HashMap::new();
-    for p in pairs.keys() { counter.insert(*p, 0); }
+    let mut counter: HashMap<[char; 2], u64>;
+    let mut next_counter: HashMap<[char; 2], u64> = HashMap::new();
+
+    // initialize counter 
+    for p in pairs.keys() { next_counter.insert(*p, 0); }
     for p in t.chars().collect::<Vec<char>>().windows(2) {
-        counter.insert([p[0], p[1]], 1); 
+        next_counter.insert([p[0], p[1]], 1);
     }
 
-    // for each pair in t, add in new entries after iteration
+    // for each character pair in t, add in new entries after iteration
     for _ in 0..n {
-        let mut next_iter_counter: HashMap<[char; 2], u64> = HashMap::new(); 
+        counter = next_counter.clone();
+        next_counter = HashMap::new(); 
 
         for (pair, count) in counter.iter() {
             if let Some([l, r]) = pairs.get(pair) {
-                let next_l = next_iter_counter.entry(*l).or_insert(0);
+                let next_l = next_counter.entry(*l).or_insert(0);
                 *next_l += count;
 
-                let next_r = next_iter_counter.entry(*r).or_insert(0);
+                let next_r = next_counter.entry(*r).or_insert(0);
                 *next_r += count;
             }
         }
 
-        counter = next_iter_counter.clone();
     }
-
-    let letter_counts: Vec<(char, u64)> = counter.iter()
+    
+    // tally starting letter of each pair, adding singular last letter from original string
+    let letter_counts = next_counter.iter()
         .map(|(k, v)| (k[0], *v))
-        .chain([(t.chars().last().unwrap(), 1)])
-        .collect();
+        .chain([(t.chars().last().unwrap(), 1)]);
 
+    // aggregate tallys by pair on first letter
     let mut letter_count_map: HashMap<char, u64> = HashMap::new();
     for (c, count) in letter_counts {
         let c_count = letter_count_map.entry(c).or_insert(0);
         *c_count += count;
     }
 
+    // score resulting polymer by component counts
     let n_most_common = letter_count_map.values().max().unwrap();
     let n_least_common = letter_count_map.values().min().unwrap();
     n_most_common - n_least_common
